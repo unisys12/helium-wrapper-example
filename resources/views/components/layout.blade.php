7ede8582b367a30f27bcdf890ecdfe62f5931704
Phillip Jackson <unisys12@gmail.com>
2021-10-27 14:37:39 -0500

    add placeholder for version

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>D2 Manifest</title>
 </head>
 <body>
@@ -12,6 +12,9 @@
     <header>
         <div class="site-title">
             <h1>D2 Manifest</h1>
+        </div>
+        <div class="version">
+            <code>version info here</code>
         </div>
     </header>
     <main>
